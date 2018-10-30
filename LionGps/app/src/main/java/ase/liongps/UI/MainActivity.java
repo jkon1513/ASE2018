c681e7acd5722e4514966d19352f77c4503cfd53
Jason Konikow <jkon1513@gmail.com>
2018-10-30 01:42:11 -0400

    Merging maps branch into master (#9)
    * restructured directories from scratch for gradle
    
    * completed setup for google services
    
    * made placeholder login page/ created map activity
    
    * created map overlay stub activity
    
    * handled all service and permission checks for location data
    
    * made some tweaks to log mssgs
    
    * added text file that stores building map data
    
    * added map fragment and method for populating building hashmap from text file
    
    * implemented map markers
    
    * centered camera and auto zoom
    
    * minor style tweak

---
+++ LionGps/app/src/main/java/ase/liongps/UI/MainActivity.java
@@ -0,0 +1,190 @@
+package ase.liongps.UI;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
+import ase.liongps.R;
+
+import static ase.liongps.utils.Constants.ERROR_DIALOG_REQUEST;
+import static ase.liongps.utils.Constants.PERMISSIONS_REQUEST_ENABLE_GPS;
+import static ase.liongps.utils.Constants.PERMISSIONS_REQUEST_FINE_LOCATION;
+
+public class MainActivity extends AppCompatActivity {
+
+    /* The design decision currently will be to get all the required permissions for GPS and
+        location services, and check for the required versions of Google Play Services
+        upon launch of the app, rather than launch of the map
+
+        in order to run we need to verify the users device has:
+
+        1. google play services
+        2. granted us permission to access location data
+        3. has gps location enabled
+     */
+
+    private final static String TAG = "loginActivity"; //will replace to loginActivity.class.getName();
+    private static boolean locationPermissionGranted = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(checkMapServices()){
+            if(locationPermissionGranted){
+                grantAccessToMap();
+            }
+            else{
+                getLocationPermission();
+            }
+        }
+    }
+
+    /* this might need to be replaced once login is implemented. for now it loads the
+        map activity once the launch button is clicked. */
+    public void loadMap(View view) {
+        Intent map = new Intent(this, mapOverlay.class);
+        startActivity(map);
+    }
+
+    // once permissions all pass this makes the launch button clickable
+    public void grantAccessToMap() {
+        Button launch = (Button) findViewById(R.id.launch);
+        launch.setClickable(true);
+        Log.d(TAG, "grantAccessToMap: launch button now clickable");
+    }
+
+
+    // PERMISSIONS ------------------------------------------------------------------
+
+    // verifies gps is enabled and google play services installed
+    private boolean checkMapServices(){
+        if(hasGoogleServices()){
+            if(hasMapsEnabled()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    //checks if the user has gps enabled on their device, if not it calls a method to help them enable
+    public boolean hasMapsEnabled(){
+        final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
+
+        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            buildAlertMessageNoGps();
+            return false;
+        }
+        return true;
+    }
+
+    // devices must have google services enabled. this verifies that fact.
+    public boolean hasGoogleServices(){
+        Log.d(TAG, "hasGoogleServices: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG, "hasGoogleServices: Google Play Services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d(TAG, "hasGoogleServices: services not found but can be retrieved");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "Your device is not compatible with LionGPS", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    // if GPS is disabled on users device, this walks them through enabling it.
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("This application requires GPS to run, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        Intent enableGpsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivityForResult(enableGpsIntent, PERMISSIONS_REQUEST_ENABLE_GPS);
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    /*
+     * Request location permission, so that we can get the location of the
+     * device. The result of the permission request is handled by a listener,
+     * onRequestPermissionsResult.
+     */
+    private void getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+            grantAccessToMap();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_FINE_LOCATION);
+        }
+    }
+
+
+
+     // Listeners / Callbacks below --------------------------------------------------------------------------------------
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        locationPermissionGranted = false;
+
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_FINE_LOCATION: {
+                // If request is somehow cancelled the array is empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true;
+                }
+            }
+        }
+    }
+
+    // this is what is called when an activity returns an intent,
+    // we can differentiate based on request code.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult: called.");
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ENABLE_GPS: {
+                if(locationPermissionGranted){
+                    grantAccessToMap();
+                }
+                else{
+                    getLocationPermission();
+                }
+            }
+        }
+
+    }
+}
---
+++ LionGps/app/src/main/java/ase/liongps/UI/mapOverlay.java
@@ -0,0 +1,78 @@
+package ase.liongps.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+import ase.liongps.R;
+
+public class mapOverlay extends AppCompatActivity implements OnMapReadyCallback {
+
+    private HashMap<String, LatLng> buildings = new HashMap<>();
+    private final String TAG = mapOverlay.class.getName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_overlay);
+
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        populateBuildings();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        //place all the markers
+        ArrayList<String> bldNames = new ArrayList<>(buildings.keySet());
+        for(String name : bldNames){
+            map.addMarker(new MarkerOptions()
+            .position(buildings.get(name))
+            .title(name));
+        }
+
+        //center camera
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(buildings.get("Lowe Library"), 18.0f));
+    }
+
+    /*
+    reads from a text file that stores the buildings and their coordinates. each line
+    contains a building name, latitude, and longitude separated by tabs. this method
+    adds each entry to the buildings hashmap using names as keys and geo-location points
+    as values. the code below is written verbosely to make this process clear.
+     */
+    public void populateBuildings() {
+        Scanner scan = new Scanner(getResources().openRawResource(R.raw.buildings));
+
+        while (scan.hasNextLine()) {
+            String[] entry = scan.nextLine().split("\t");
+            String name = entry[0];
+            double lat = Double.parseDouble(entry[1]);
+            double lng = Double.parseDouble(entry[2]);
+
+            buildings.put(name, new LatLng(lat,lng));
+
+            // convert to unit test
+            Log.d(TAG,"populateBuildings >> buidling name: " + name);
+            Log.d(TAG,"populateBuildings >> buidling lat: " + lat);
+            Log.d(TAG,"populateBuildings >> buidling lng: " + lng);
+            Log.d(TAG,"populateBuildings >> result of map.get: " + buildings.get(name));
+        }
+
+    }
+
+
+}
