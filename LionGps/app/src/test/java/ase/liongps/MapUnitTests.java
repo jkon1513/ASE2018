50760cb16c7f7dfffe1dcd70992a8c8b79723396
Luigi-PastorePica <luigi.pastorepica@outlook.com>
2018-10-30 02:26:27 -0400

    Added Travis-CI settings, modified other files to set-up gradle for tests

---
+++ LionGps/app/src/test/java/ase/liongps/MapUnitTests.java
@@ -0,0 +1,14 @@
+package ase.liongps;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+//import com.google.common.truth.Truth.assertThat;
+
+public class MapUnitTests {
+    @Test
+    public void firstTest(){
+        assertEquals("failure - strings are not equal", "text", "text");
+    }
+}
