4d69238b00098ad45b7ccd3f455b9d2c3fe2e88e
Jkon1513 <jkon1513@gmail.com>
2018-11-21 01:32:26 -0500

    improved modularity of search interactor

--- LionGps/app/src/main/java/ase/liongps/MapOverlay/SearchInteractor.java
+++ LionGps/app/src/main/java/ase/liongps/MapOverlay/SearchInteractor.java
@@ -4,40 +4,28 @@ import ase.liongps.utils.Building;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Scanner;
 
 public class SearchInteractor {
-    private HashMap <String , Building> buildings;
+    private HashMap<String, Building> buildings;
 
     public SearchInteractor() {
         buildings = new HashMap<>();
-        populateBuildings();
     }
 
 
-    /*
-   reads from a text file that stores the buildings and their coordinates. each line
-   contains a building name, latitude, and longitude separated by tabs. this method
-   adds each entry to the buildings hashmap using names as keys and Building objects as vals.
-    */
-    private void populateBuildings(){
+    public void populateBuildings(String fileEntry) {
+        String[] bldngData = fileEntry.split("\t");
 
-        String file = "res/raw/buildings.txt";
-        Scanner scan = new Scanner(this.getClass().getClassLoader().getResourceAsStream(file));
+        String name = bldngData[0];
+        double lat = Double.parseDouble(bldngData[1]);
+        double lng = Double.parseDouble(bldngData[2]);
 
-        while (scan.hasNextLine()) {
-            String[] entry = scan.nextLine().split("\t");
-            String name = entry[0];
-            double lat = Double.parseDouble(entry[1]);
-            double lng = Double.parseDouble(entry[2]);
-
-            buildings.put(name, new Building(name, lat, lng));
-        }
+        buildings.put(name, new Building(name, lat, lng));
     }
 
-    public boolean isValidSearch(String query){
-        return buildings.containsKey(query);
+    public boolean isValidSearch(String query) {
+        return buildings.containsKey(query.toLowerCase().trim());
     }
 
     //returns the requested building object, error handling in presenter
@@ -45,8 +33,24 @@ public class SearchInteractor {
         return buildings.get(name);
     }
 
-    public ArrayList<String> getValidBuildings(){
+    public ArrayList<String> getValidBuildings() {
         return new ArrayList<>(buildings.keySet());
     }
 
+
+    /*
+    reads from a text file that stores the buildings and their coordinates. each line
+    contains a building name, latitude, and longitude separated by tabs. this method
+    adds each entry to the buildings hashmap using names as keys and Building objects as vals.
+    */
+    protected void readBuildingData() {
+        String file = "res/raw/buildings.txt";
+        Scanner scan = new Scanner(this.getClass().getClassLoader().getResourceAsStream(file));
+
+        while (scan.hasNextLine()) {
+            String entry = scan.nextLine();
+            populateBuildings(entry);
+        }
+
+    }
 }
