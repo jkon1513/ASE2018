ad35edd1126410e7a80444548d23576d1e3b06e4
Luigi-PastorePica <luigi.pastorepica@outlook.com>
2018-12-15 19:14:33 -0500

    Tests added for ProfilePageInteractor

---
+++ LionGps/app/src/test/java/ase/liongps/test/ProfilePageInteractorTest.java
@@ -0,0 +1,73 @@
+package ase.liongps.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.BeforeClass;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Arrays;
+import java.util.Iterator;
+
+import ase.liongps.ProfilePage.ProfilePageInteractor;
+
+public class ProfilePageInteractorTest {
+
+    private static ProfilePageInteractor profileInteractor;
+    private static HashSet<String> mockSchedule;
+    private static Set<String> mockScheduleSet;
+    private static HashSet<String> nullSchedule;
+    //private static Iterator mockScheduleIterator;
+
+    @BeforeClass
+    public static void setup() {
+
+        profileInteractor = new ProfilePageInteractor();
+
+        String[] scheduleEntries = {
+                "History of Ribbon Dancing : Dodge Physical Fitness Center",
+                "History of Metal Music : Butler Library",
+                "Flea Market Economics : Uris Hall",
+                "Cold Fusion 1 : Pupin Laboratories",
+                "Bailing Criminals : William and June Warren Hall"};
+
+        mockSchedule = new HashSet<String>(Arrays.asList(scheduleEntries));
+
+    }
+
+    @Test
+    public void addToScheduleTest() {
+
+        for (String mockScheduleEntry : mockSchedule) {
+            Assert.assertTrue("Entry \"" +  mockScheduleEntry + "\" not added to schedule",
+                    profileInteractor.addToSchedule(mockScheduleEntry));
+        }
+        Assert.assertFalse("null object added to schedule",
+                profileInteractor.addToSchedule(null));
+
+    }
+
+    @Test
+    public void getScheduleTest(){
+
+        HashSet<String> storedSchedule = profileInteractor.getSchedule();
+
+        for(String mockScheduleEntry : mockSchedule){
+            Assert.assertTrue("Entry \"" + "\" was not present in the schedule",
+                    storedSchedule.contains(mockScheduleEntry));
+        }
+    }
+
+    @Test
+    public void loadScheduleTest(){
+
+        nullSchedule = null;
+
+        Assert.assertTrue("courseList not loaded",
+                profileInteractor.loadSchedule(mockSchedule));
+
+        Assert.assertFalse("null courseList loaded",
+                profileInteractor.loadSchedule(nullSchedule));
+    }
+
+}
